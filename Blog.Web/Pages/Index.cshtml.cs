83584e58b7fe1da36c1fa9661adae7d305af44e7
Anurag Sinha <anurag3487@gmail.com>
2024-02-17 22:51:07 +0530

    Added Blog List

--- Blog.Web/Pages/Index.cshtml.cs
+++ Blog.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Blog.Web.Pages;
@@ -6,13 +8,35 @@ namespace Blog.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly BlogDbContext _dbContext;
+    
+    [BindProperty]
+    public List<BlogViewModel> Blogs { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public bool HasRecords
+    {
+        get
+        {
+            return Blogs.Count > 0;
+        }
+    }
+    public IndexModel(ILogger<IndexModel> logger, BlogDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public void OnGet()
     {
+        var data = _dbContext.Blogs.ToList();
+        Blogs = data.Select(x => new BlogViewModel
+        {
+            Guid = x.Guid,
+            Contents = x.Contents,
+            Title = x.Title,
+            ImageUrl = x.ImageUrl,
+            PublishedDate = x.PublishedDate,
+            IsActive = x.IsActive
+        }).ToList();
     }
 }
